71caeec5253c50bf76000df26f5f5ea4ef2b945e
Austin Cummings and Ben Kiggen <austin@>
2018-08-07 15:39:00 -0700

    first commit, last commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,7 @@
 $(document).ready(function() {
-
-  $("button#hello").click(function() {
-    $("ul#user").before("<li>Hello!</li>");
-    $("ul#webpage").before("<li>Why hello there!</li>");
-
-    $("ul#user").children("li").first().click(function() {
-      $(this).remove();
-      alert(this.class);
-    });
-    $("ul#webpage").children("li").first().click(function() {
-      $(this).remove();
-    });
-  });
-
-  $("button#goodbye").click(function() {
-    $("ul#user").before("<li>Goodbye!</li>");
-    $("ul#webpage").before("<li>Goodbye, dear user!</li>");
-    $("ul#user").children("li").first().click(function() {
-      $(this).remove();
-    });
-    $("ul#webpage").children("li").first().click(function() {
-      $(this).remove();
-    });
-  });
-
-  $("button#stop").click(function() {
-    $("ul#user").before("<li>Stop copying me!</li>");
-    $("ul#webpage").before("<li>Pardon me. I meant no offense.</li>");
-    $("ul#user").children("li").first().click(function() {
-      $(this).remove();
-    });
-    $("ul#webpage").children("li").first().click(function() {
-      $(this).remove();
-    });
+  // $(alert("asdf"));
+  $(".col-md-4").click(function(){
+    $(this).children("p").toggle();
+    $(this).children("img").toggleClass("imagestyle2");
   });
 });
