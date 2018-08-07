4ce0e2facc524370b5136dd2794e4bf68d1b1516
Austin Cummings and Ben Kiggen <austin@>
2018-08-07 12:00:19 -0700

    add flashcard content

--- js/scripts.js
+++ js/scripts.js
@@ -1,81 +1,81 @@
 
 $(document).ready(function(){
   $(".row1 .col1").click(function(){
-    $(".row1 .col1 .answer-hidden").toggle();
-    $(".row1 .col1 .answer-showing").toggle();
+    $(".row1 .col1 .answer-hidden").slideToggle("swing slow");
+    $(".row1 .col1 .answer-showing").slideToggle("swing slow");
   });
   $(".row1 .col2").click(function(){
-    $(".row1 .col2 .answer-hidden").toggle();
-    $(".row1 .col2 .answer-showing").toggle();
+    $(".row1 .col2 .answer-hidden").slideToggle();
+    $(".row1 .col2 .answer-showing").slideToggle();
   });
   $(".row1 .col3").click(function(){
-    $(".row1 .col3 .answer-hidden").toggle();
-    $(".row1 .col3 .answer-showing").toggle();
+    $(".row1 .col3 .answer-hidden").slideToggle();
+    $(".row1 .col3 .answer-showing").slideToggle();
   });
   $(".row1 .col4").click(function(){
-    $(".row1 .col4 .answer-hidden").toggle();
-    $(".row1 .col4 .answer-showing").toggle();
+    $(".row1 .col4 .answer-hidden").slideToggle();
+    $(".row1 .col4 .answer-showing").slideToggle();
   });
 
   $(".row2 .col1").click(function(){
-    $(".row2 .col1 .answer-hidden").toggle();
-    $(".row2 .col1 .answer-showing").toggle();
+    $(".row2 .col1 .answer-hidden").slideToggle();
+    $(".row2 .col1 .answer-showing").slideToggle();
   });
   $(".row2 .col2").click(function(){
-    $(".row2 .col2 .answer-hidden").toggle();
-    $(".row2 .col2 .answer-showing").toggle();
+    $(".row2 .col2 .answer-hidden").slideToggle();
+    $(".row2 .col2 .answer-showing").slideToggle();
   });
   $(".row2 .col3").click(function(){
-    $(".row2 .col3 .answer-hidden").toggle();
-    $(".row2 .col3 .answer-showing").toggle();
+    $(".row2 .col3 .answer-hidden").slideToggle();
+    $(".row2 .col3 .answer-showing").slideToggle();
   });
   $(".row2 .col4").click(function(){
-    $(".row2 .col4 .answer-hidden").toggle();
-    $(".row2 .col4 .answer-showing").toggle();
+    $(".row2 .col4 .answer-hidden").slideToggle();
+    $(".row2 .col4 .answer-showing").slideToggle();
   });
 
   $(".row3 .col1").click(function(){
-    $(".row3 .col1 .answer-hidden").toggle();
-    $(".row3 .col1 .answer-showing").toggle();
+    $(".row3 .col1 .answer-hidden").slideToggle();
+    $(".row3 .col1 .answer-showing").slideToggle();
   });
   $(".row3 .col2").click(function(){
-    $(".row3 .col2 .answer-hidden").toggle();
-    $(".row3 .col2 .answer-showing").toggle();
+    $(".row3 .col2 .answer-hidden").slideToggle();
+    $(".row3 .col2 .answer-showing").slideToggle();
   });
   $(".row3 .col3").click(function(){
-    $(".row3 .col3 .answer-hidden").toggle();
-    $(".row3 .col3 .answer-showing").toggle();
+    $(".row3 .col3 .answer-hidden").slideToggle();
+    $(".row3 .col3 .answer-showing").slideToggle();
   });
   $(".row3 .col4").click(function(){
-    $(".row3 .col4 .answer-hidden").toggle();
-    $(".row3 .col4 .answer-showing").toggle();
+    $(".row3 .col4 .answer-hidden").slideToggle();
+    $(".row3 .col4 .answer-showing").slideToggle();
   });
 
   $(".row4 .col1").click(function(){
-    $(".row4 .col1 .answer-hidden").toggle();
-    $(".row4 .col1 .answer-showing").toggle();
+    $(".row4 .col1 .answer-hidden").slideToggle();
+    $(".row4 .col1 .answer-showing").slideToggle();
   });
   $(".row4 .col2").click(function(){
-    $(".row4 .col2 .answer-hidden").toggle();
-    $(".row4 .col2 .answer-showing").toggle();
+    $(".row4 .col2 .answer-hidden").slideToggle();
+    $(".row4 .col2 .answer-showing").slideToggle();
   });
   $(".row4 .col3").click(function(){
-    $(".row4 .col3 .answer-hidden").toggle();
-    $(".row4 .col3 .answer-showing").toggle();
+    $(".row4 .col3 .answer-hidden").slideToggle();
+    $(".row4 .col3 .answer-showing").slideToggle();
   });
   $(".row4 .col4").click(function(){
-    $(".row4 .col4 .answer-hidden").toggle();
-    $(".row4 .col4 .answer-showing").toggle();
+    $(".row4 .col4 .answer-hidden").slideToggle();
+    $(".row4 .col4 .answer-showing").slideToggle();
   });
 
 
-  $(".row5 .col1").click(function(){
-    $(".row5 .col1 .answer-hidden").toggle();
-    $(".row5 .col1 .answer-showing").toggle();
-  });
   $(".row5 .col2").click(function(){
-    $(".row5 .col2 .answer-hidden").toggle();
-    $(".row5 .col2 .answer-showing").toggle();
+    $(".row5 .col2 .answer-hidden").slideToggle();
+    $(".row5 .col2 .answer-showing").slideToggle();
+  });
+  $(".row5 .col3").click(function(){
+    $(".row5 .col3 .answer-hidden").slideToggle();
+    $(".row5 .col3 .answer-showing").slideToggle();
   });
 
 });
