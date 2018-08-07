6ada7bbf3cb2fa1cd5e1174cf1fb53ca020d6928
Austin Cummings and Ben Kiggen <austin@>
2018-08-07 11:26:27 -0700

    first commit, basic functionality there, some styling and html still needed.

---
+++ js/scripts.js
@@ -0,0 +1,81 @@
+
+$(document).ready(function(){
+  $(".row1 .col1").click(function(){
+    $(".row1 .col1 .answer-hidden").toggle();
+    $(".row1 .col1 .answer-showing").toggle();
+  });
+  $(".row1 .col2").click(function(){
+    $(".row1 .col2 .answer-hidden").toggle();
+    $(".row1 .col2 .answer-showing").toggle();
+  });
+  $(".row1 .col3").click(function(){
+    $(".row1 .col3 .answer-hidden").toggle();
+    $(".row1 .col3 .answer-showing").toggle();
+  });
+  $(".row1 .col4").click(function(){
+    $(".row1 .col4 .answer-hidden").toggle();
+    $(".row1 .col4 .answer-showing").toggle();
+  });
+
+  $(".row2 .col1").click(function(){
+    $(".row2 .col1 .answer-hidden").toggle();
+    $(".row2 .col1 .answer-showing").toggle();
+  });
+  $(".row2 .col2").click(function(){
+    $(".row2 .col2 .answer-hidden").toggle();
+    $(".row2 .col2 .answer-showing").toggle();
+  });
+  $(".row2 .col3").click(function(){
+    $(".row2 .col3 .answer-hidden").toggle();
+    $(".row2 .col3 .answer-showing").toggle();
+  });
+  $(".row2 .col4").click(function(){
+    $(".row2 .col4 .answer-hidden").toggle();
+    $(".row2 .col4 .answer-showing").toggle();
+  });
+
+  $(".row3 .col1").click(function(){
+    $(".row3 .col1 .answer-hidden").toggle();
+    $(".row3 .col1 .answer-showing").toggle();
+  });
+  $(".row3 .col2").click(function(){
+    $(".row3 .col2 .answer-hidden").toggle();
+    $(".row3 .col2 .answer-showing").toggle();
+  });
+  $(".row3 .col3").click(function(){
+    $(".row3 .col3 .answer-hidden").toggle();
+    $(".row3 .col3 .answer-showing").toggle();
+  });
+  $(".row3 .col4").click(function(){
+    $(".row3 .col4 .answer-hidden").toggle();
+    $(".row3 .col4 .answer-showing").toggle();
+  });
+
+  $(".row4 .col1").click(function(){
+    $(".row4 .col1 .answer-hidden").toggle();
+    $(".row4 .col1 .answer-showing").toggle();
+  });
+  $(".row4 .col2").click(function(){
+    $(".row4 .col2 .answer-hidden").toggle();
+    $(".row4 .col2 .answer-showing").toggle();
+  });
+  $(".row4 .col3").click(function(){
+    $(".row4 .col3 .answer-hidden").toggle();
+    $(".row4 .col3 .answer-showing").toggle();
+  });
+  $(".row4 .col4").click(function(){
+    $(".row4 .col4 .answer-hidden").toggle();
+    $(".row4 .col4 .answer-showing").toggle();
+  });
+
+
+  $(".row5 .col1").click(function(){
+    $(".row5 .col1 .answer-hidden").toggle();
+    $(".row5 .col1 .answer-showing").toggle();
+  });
+  $(".row5 .col2").click(function(){
+    $(".row5 .col2 .answer-hidden").toggle();
+    $(".row5 .col2 .answer-showing").toggle();
+  });
+
+});
