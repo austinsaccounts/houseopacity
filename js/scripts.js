576aea6d1c9c807019cd03f19e21121cfd723d04
Austin Cummings and Ben Kiggen <ben@>
2018-08-07 14:18:22 -0700

    started the begining

--- js/scripts.js
+++ js/scripts.js
@@ -1,81 +1,37 @@
+$(document).ready(function() {
 
-$(document).ready(function(){
-  $(".row1 .col1").click(function(){
-    $(".row1 .col1 .answer-hidden").slideToggle("swing slow");
-    $(".row1 .col1 .answer-showing").slideToggle("swing slow");
-  });
-  $(".row1 .col2").click(function(){
-    $(".row1 .col2 .answer-hidden").slideToggle();
-    $(".row1 .col2 .answer-showing").slideToggle();
-  });
-  $(".row1 .col3").click(function(){
-    $(".row1 .col3 .answer-hidden").slideToggle();
-    $(".row1 .col3 .answer-showing").slideToggle();
-  });
-  $(".row1 .col4").click(function(){
-    $(".row1 .col4 .answer-hidden").slideToggle();
-    $(".row1 .col4 .answer-showing").slideToggle();
+  $("button#hello").click(function() {
+    $("ul#user").before("<li>Hello!</li>");
+    $("ul#webpage").before("<li>Why hello there!</li>");
+
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+      alert(this.class);
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+    });
   });
 
-  $(".row2 .col1").click(function(){
-    $(".row2 .col1 .answer-hidden").slideToggle();
-    $(".row2 .col1 .answer-showing").slideToggle();
-  });
-  $(".row2 .col2").click(function(){
-    $(".row2 .col2 .answer-hidden").slideToggle();
-    $(".row2 .col2 .answer-showing").slideToggle();
-  });
-  $(".row2 .col3").click(function(){
-    $(".row2 .col3 .answer-hidden").slideToggle();
-    $(".row2 .col3 .answer-showing").slideToggle();
-  });
-  $(".row2 .col4").click(function(){
-    $(".row2 .col4 .answer-hidden").slideToggle();
-    $(".row2 .col4 .answer-showing").slideToggle();
+  $("button#goodbye").click(function() {
+    $("ul#user").before("<li>Goodbye!</li>");
+    $("ul#webpage").before("<li>Goodbye, dear user!</li>");
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+    });
   });
 
-  $(".row3 .col1").click(function(){
-    $(".row3 .col1 .answer-hidden").slideToggle();
-    $(".row3 .col1 .answer-showing").slideToggle();
+  $("button#stop").click(function() {
+    $("ul#user").before("<li>Stop copying me!</li>");
+    $("ul#webpage").before("<li>Pardon me. I meant no offense.</li>");
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
+    });
   });
-  $(".row3 .col2").click(function(){
-    $(".row3 .col2 .answer-hidden").slideToggle();
-    $(".row3 .col2 .answer-showing").slideToggle();
-  });
-  $(".row3 .col3").click(function(){
-    $(".row3 .col3 .answer-hidden").slideToggle();
-    $(".row3 .col3 .answer-showing").slideToggle();
-  });
-  $(".row3 .col4").click(function(){
-    $(".row3 .col4 .answer-hidden").slideToggle();
-    $(".row3 .col4 .answer-showing").slideToggle();
-  });
-
-  $(".row4 .col1").click(function(){
-    $(".row4 .col1 .answer-hidden").slideToggle();
-    $(".row4 .col1 .answer-showing").slideToggle();
-  });
-  $(".row4 .col2").click(function(){
-    $(".row4 .col2 .answer-hidden").slideToggle();
-    $(".row4 .col2 .answer-showing").slideToggle();
-  });
-  $(".row4 .col3").click(function(){
-    $(".row4 .col3 .answer-hidden").slideToggle();
-    $(".row4 .col3 .answer-showing").slideToggle();
-  });
-  $(".row4 .col4").click(function(){
-    $(".row4 .col4 .answer-hidden").slideToggle();
-    $(".row4 .col4 .answer-showing").slideToggle();
-  });
-
-
-  $(".row5 .col2").click(function(){
-    $(".row5 .col2 .answer-hidden").slideToggle();
-    $(".row5 .col2 .answer-showing").slideToggle();
-  });
-  $(".row5 .col3").click(function(){
-    $(".row5 .col3 .answer-hidden").slideToggle();
-    $(".row5 .col3 .answer-showing").slideToggle();
-  });
-
 });
